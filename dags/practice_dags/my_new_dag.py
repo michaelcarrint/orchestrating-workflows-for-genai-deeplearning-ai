ccdabda16a6ab0400be0edae9ac8eabb4aa27875
michaelcarrint <Michaelcarrington_2024@outlook.com>
2025-07-10 16:30:20 +0100

    Add my_new_dag.py

---
+++ dags/practice_dags/my_new_dag.py
@@ -0,0 +1,20 @@
+
+from airflow.sdk import dag, task
+
+@dag
+def my_new_dag():
+
+    @task
+    def my_task_1():
+        return {"my_world": "Airflow!"}
+
+    _my_task_1 = my_task_1()
+
+    @task
+    def my_task_2():
+        print(my_dict["my_word"])
+
+    _my_task_2 = my_task_2(my_dict=_my_task1) 
+
+
+my_new_dag()
