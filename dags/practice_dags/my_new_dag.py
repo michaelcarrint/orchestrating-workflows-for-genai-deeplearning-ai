b888029a7c4177ec5caff295fff03bc3c48aca93
michaelcarrint <Michaelcarrington_2024@outlook.com>
2025-07-11 20:16:20 +0100

    Add new task to my_new_dag

--- dags/practice_dags/my_new_dag.py
+++ dags/practice_dags/my_new_dag.py
@@ -12,9 +12,16 @@ def my_new_dag():
     def my_task_2(data):
         print(data["my_word"])
 
+    @task
+    def my_task_3():
+        print("Hi, from task 3")
+        
+
     # Call tasks
     _my_task_1 = my_task_1()
-    _my_task_2 = my_task_2(_my_task_1)  # ✅ pass as positional arg
+    _my_task_2 = my_task_2(_my_task_1) # ✅ pass as positional arg
+    _my_task_3 = my_task_3()
 
+    chain(_my_task_1, _my_task_3)
+    
 my_new_dag()
-
